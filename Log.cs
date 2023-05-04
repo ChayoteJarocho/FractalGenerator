889c2d7ce580ddbe61acbacd61aea39b92546813
ChayoteJarocho <77412126+ChayoteJarocho@users.noreply.github.com>
2023-05-04 00:26:28 -0700

    Simplify Log.cs

--- Log.cs
+++ Log.cs
@@ -5,55 +5,25 @@ namespace FractalGenerator
 {
     public class Log
     {
-        public static void Line()
-        {
-            Console.WriteLine();
-        }
+        public static void Line() => Console.WriteLine();
 
-        public static void Info(string format, params object[] args)
-        {
-            Info(true, format, args);
-        }
+        public static void Info(string format, params object[] args) => Info(true, format, args);
 
-        public static void Info(bool endline, string format, params object[] args)
-        {
-            Print(endline, ConsoleColor.White, format, args);
-        }
+        public static void Info(bool endline, string format, params object[] args) => Print(endline, ConsoleColor.White, format, args);
 
-        public static void Success(string format, params object[] args)
-        {
-            Success(true, format, args);
-        }
+        public static void Success(string format, params object[] args) => Success(true, format, args);
 
-        public static void Success(bool endline, string format, params object[] args)
-        {
-            Print(endline, ConsoleColor.Green, format, args);
-        }
+        public static void Success(bool endline, string format, params object[] args) => Print(endline, ConsoleColor.Green, format, args);
 
-        public static void Warning(string format, params object[] args)
-        {
-            Warning(true, format, args);
-        }
+        public static void Warning(string format, params object[] args) => Warning(true, format, args);
 
-        public static void Warning(bool endline, string format, params object[] args)
-        {
-            Print(endline, ConsoleColor.Yellow, format, args);
-        }
+        public static void Warning(bool endline, string format, params object[] args) => Print(endline, ConsoleColor.Yellow, format, args);
 
-        public static void Error(string format, params object[] args)
-        {
-            Error(true, format, args);
-        }
+        public static void Error(string format, params object[] args) => Error(true, format, args);
 
-        public static void Error(bool endline, string format, params object[] args)
-        {
-            Print(endline, ConsoleColor.Red, format, args);
-        }
+        public static void Error(bool endline, string format, params object[] args) => Print(endline, ConsoleColor.Red, format, args);
 
-        public static void Assert(bool condition, string format, params object[] args)
-        {
-            Assert(true, condition, format, args);
-        }
+        public static void Assert(bool condition, string format, params object[] args) => Assert(true, condition, format, args);
 
         public static void Assert(bool endline, bool condition, string format, params object[] args)
         {
